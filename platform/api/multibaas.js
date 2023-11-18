bbc99a64b7a8ee19f5521c80ef0823122633fb19
Denis Perov <denis.perov@gmail.com>
2023-11-18 15:11:59 +0300

    Support approving & rejecting of Agents via self-signed transactions

--- platform/api/multibaas.js
+++ platform/api/multibaas.js
@@ -12,9 +12,51 @@ const config = new MultiBaas.Configuration({
 const chain = 'ethereum';
 
 export const Chains = new MultiBaas.ChainsApi(config);
+export const Contracts = new MultiBaas.ContractsApi(config);
 export const Events = new MultiBaas.EventsApi(config);
 
-Events.listEvents();
+export function convertMultiBaasError(err) {
+  return new Meteor.Error(
+    err.response?.status ?? 503,
+    err.response?.data?.message ?? err.message,
+  );
+}
+
+export async function getChainId() {
+  const { data } = await Chains.getChainStatus('ethereum');
+  return data.result.chainID;
+}
+
+export async function prepateAndSubmitContractWriteFunction(contract, name, args, signer) {
+  const { data: { result: { tx } } } = await Contracts.callContractFunction(
+    'ethereum',
+    contract.address,
+    contract.label,
+    'agentApprove',
+    {
+      from: signer.address,
+      args,
+    },
+  );
+  const signed = await signer.signTransaction({
+    from: signer.address,
+    chainId: await getChainId(),
+    to: tx.to,
+    data: tx.data,
+    value: tx.value,
+    nonce: tx.nonce,
+    gasLimit: tx.gas,
+    // maxFeePerGas: Number(tx.gasFeeCap),
+    // maxPriorityFeePerGas: Number(tx.gasTipCap),
+    type: tx.type,
+  });
+  try {
+    const { data } = await Chains.submitSignedTransaction('ethereum', { signedTx: signed });
+    return data.result;
+  } catch (err) {
+    throw convertMultiBaasError(err);
+  }
+}
 
 Meteor.startup(async () => {
   const { data } = await Chains.getChainStatus(chain);
