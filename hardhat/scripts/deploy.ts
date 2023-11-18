3e6c95193c936a291549f11f678f8093a5931d40
arjanjohan <arjanvanhoogdalem@gmail.nl>
2023-11-18 09:59:04 +0100

    hardhat deployment

--- hardhat/scripts/deploy.ts
+++ hardhat/scripts/deploy.ts
@@ -1,27 +1,49 @@
+//This script deploys the ERC20Votes contract to the Goerli testnet
 import { ethers } from "hardhat";
+import * as dotenv from "dotenv";
+
+
+// to deploy use:
+// npx hardhat run scripts/deploy.ts --network goerli 
+// npx hardhat run scripts/deploy.ts --network alfajores
+// npx hardhat run scripts/deploy.ts --network linea
+// npx hardhat run scripts/deploy.ts --network chiado
+// npx hardhat run scripts/deploy.ts --network baseGoerli
+// npx hardhat run scripts/deploy.ts --network arbitrumGoerli
+// npx hardhat run scripts/deploy.ts --network mantleTestnet
+
+// no funds:
+// npx hardhat run scripts/deploy.ts --network scrollSepolia
+
+
+// npx hardhat verify --network scroll 0x04b3786899D4400bBEf2f000c07CBB916a9a8E24 0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889 200 0xAfAE2dD69F115ec26DFbE2fa5a8642D94D7Cd37E
+// npx hardhat verify --network mantleTestnet 0x88403E5719295B76a1A456b9B2665aCDA9AD4943 0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889 200 0xAfAE2dD69F115ec26DFbE2fa5a8642D94D7Cd37E
+dotenv.config();
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = ethers.parseEther("0.001");
-
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  let contract;
+  try {
+    contract = await ethers.deployContract("Help", ["0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889", 200, "0xAfAE2dD69F115ec26DFbE2fa5a8642D94D7Cd37E"]);
+  }
+  catch (error) {
+    // In case of error try with specific config for mantleTestnet
+    try {contract = await ethers.deployContract("Help", ["0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889", 200, "0xAfAE2dD69F115ec26DFbE2fa5a8642D94D7Cd37E"], { gasLimit: 10000000});}
+    catch (error) {
+      console.error('An error occurred during contract deployment:', error);
+      process.exit(1); // Exit with an error code
+  }
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+
+  console.log("Contract address:", await contract.getAddress());
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+
+  // 0x388615CDF39FE5934Ac05AcFf2bbB86DB933CcB7
