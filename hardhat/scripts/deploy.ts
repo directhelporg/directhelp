5afc803fc70a3e5fb16ea43f318379ed3a8b6873
arjanjohan <arjanvanhoogdalem@gmail.nl>
2023-11-18 16:24:46 +0100

    deploy script fix

--- hardhat/scripts/deploy.ts
+++ hardhat/scripts/deploy.ts
@@ -28,9 +28,10 @@ import {BaseContract} from "ethers";
 dotenv.config();
 
 async function main() {
-	const arg1 = "0x1F4dC6D69E3b4dAC139E149E213a7e863a813466"; // BASE Goerli UMA contract
-	const arg2 = 200;
-	const arg3 = "0xEF8b46765ae805537053C59f826C3aD61924Db45"; // BASE Goerli WETH ERC20 
+	const uma_address = "0x1F4dC6D69E3b4dAC139E149E213a7e863a813466"; // BASE Goerli UMA contract
+	const dispute_time = 200;
+	const currency = "0xEF8b46765ae805537053C59f826C3aD61924Db45"; // BASE Goerli WETH ERC20 
+	const eas_address = "0x4200000000000000000000000000000000000021"
 
 	const contractVersion = "1.2.12";
 
@@ -40,19 +41,19 @@ async function main() {
 		contract = await deployWithMB(
 			"Help",
 			contractVersion,
-			[arg1, arg2, arg3],
+			[uma_address, dispute_time, currency, eas_address],
 			(await ethers.getSigners())[0]);
 	}
 	if(hre.network.name != "mantleTestnet") {
-		contract = await ethers.deployContract("Help", [arg1, arg2, arg3]);
+		contract = await ethers.deployContract("Help", [uma_address, dispute_time, currency, eas_address]);
 	} else {
-		contract = await ethers.deployContract("Help", [arg1, arg2, arg3], {gasLimit: 10000000});
+		contract = await ethers.deployContract("Help", [uma_address, dispute_time, currency, eas_address], {gasLimit: 10000000});
 	}
 
 	const deployContractAddress = await contract.getAddress();
 	console.log("Contract address:", deployContractAddress);
 	console.log("To verify execute the command below:");
-	console.log("npx hardhat verify --network " + hre.network.name + " " + deployContractAddress + " " + arg1 + " " + arg2 + " " + arg3);
+	console.log("npx hardhat verify --network " + hre.network.name + " " + deployContractAddress + " " + uma_address + " " + dispute_time + " " + currency + " " + eas_address);
 }
 
 async function deployWithMB(
